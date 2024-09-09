91f9dd3a08100052b69ae2db5baa2b93a568b756
Aakarsh Verma <133818110+abcd-aakarsh@users.noreply.github.com>
2024-09-10 04:15:43 +0530

    fixed deployment issue

--- src/App.jsx
+++ src/App.jsx
@@ -7,20 +7,18 @@ import ProductDetailsPage from "./pages/ProductDetailsPage";
 
 const App = () => {
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route element={<AppLayout />}>
-            <Route index element={<Navigate replace to="home" />} />
-            <Route path="home" element={<HomePage />} />
-            <Route path="products" element={<ProductsPage />} />
-            <Route path="products/:id" element={<ProductDetailsPage />} />
-          </Route>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<AppLayout />}>
+          <Route index element={<Navigate replace to="home" />} />
+          <Route path="home" element={<HomePage />} />
+          <Route path="products" element={<ProductsPage />} />
+          <Route path="products/:id" element={<ProductDetailsPage />} />
+        </Route>
 
-          <Route path="*" element={<PageNotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </>
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
